1611e8b5d13841030060d8c864fd76f017dfd278
anitta08 <142616421+anitta08@users.noreply.github.com>
2024-09-23 12:03:27 -0300

    3

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ console.log(areaQuadrado(6))
 let areaTriangulo=function(base,altura){
  return (base*altura/2)
 }
-console.log(areaTriangulo(13,19))
+console.log(areaTriangulo(10,20))
 
 let nome=prompt('?');
 document.write(nome);
