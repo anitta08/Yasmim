fca6ba5bc9864cae2b3a5e94d04aaa1abdf8995e
anitta08 <142616421+anitta08@users.noreply.github.com>
2024-09-30 11:20:56 -0300

    rt

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-  // let nomeUsuario = 'yasmin chevonica';
-  // alert (nomeUsuario);
-function soma(num1,num2){
-  return num1+num2;
-}
-console.log(soma (1,2));
-function areaQuadrado(lado){
-    return lado**2
-}
-console.log(areaQuadrado(6))
+//   // let nomeUsuario = 'yasmin chevonica';
+//   // alert (nomeUsuario);
+// function soma(num1,num2){
+//   return num1+num2;
+// }
+// console.log(soma (1,2));
+// function areaQuadrado(lado){
+//     return lado**2
+// }
+// console.log(areaQuadrado(6))
 
 
-let areaTriangulo=function(base,altura){
- return (base*altura/2)
-}
-console.log(areaTriangulo(10,20))
+// let areaTriangulo=function(base,altura){
+//  return (base*altura/2)
+// }
+// console.log(areaTriangulo(10,20))
 
-let mediaAritmetica1=parseInt(prompt('numero 1'));
-let mediaAritmetica2=parseInt(prompt('numero 2'));
-let mediaAritmetica3=parseInt(prompt('numero 3'));
-function media(num1,num2,num3){
-    return(num1+num2+num3)/3;
-}
-document.write(media(mediaAritmetica1,mediaAritmetica2,mediaAritmetica3));
+// let mediaAritmetica1=parseInt(prompt('numero 1'));
+// let mediaAritmetica2=parseInt(prompt('numero 2'));
+// let mediaAritmetica3=parseInt(prompt('numero 3'));
+// function media(num1,num2,num3){
+//     return(num1+num2+num3)/3;
+// }
+// document.write(media(mediaAritmetica1,mediaAritmetica2,mediaAritmetica3));
+
+let estados=Parana', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
