7e5fe3f4f2877d2ba7039a191e6b932b3e59ed60
anitta08 <142616421+anitta08@users.noreply.github.com>
2024-09-30 11:52:53 -0300

    9yhn,

--- main.js
+++ main.js
@@ -25,7 +25,7 @@
 
 let estados = ['Parana', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
 let i = 0;
-while(i<10){
+while(i<26){
 console.log(i);
 console.log(estados[i]);
 i=i+1;}
